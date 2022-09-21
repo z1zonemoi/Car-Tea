9bb61627a3a7253155d8ff56eb03dc970035827e
z1zonemoi <zizonemoi@gmail.com>
2022-09-21 21:52:52 +0900

    nav

--- tea/src/App.js
+++ tea/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Nav from './components/Nav';
+
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
-  );
+    <>
+    <Nav/>
+    </>
+  )
 }
 
 export default App;
---
+++ tea/src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {RiCupFill} from 'react-icons/ri'
+
+const Nav = () => {
+    return (
+        <header>
+            <nav>
+                <RiCupFill/> Car 말고 Tea
+            </nav>
+        </header>
+    );
+};
+
+export default Nav;
